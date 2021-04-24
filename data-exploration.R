4bb8621c1e723da6f8633d6cbf884f20c3e2a1ba
Charmaine Runes <charmaine.runes@gmail.com>
2021-04-24 10:04:38 -0500

    added two charts

--- data-exploration.R
+++ data-exploration.R
@@ -11,6 +11,7 @@ library(stringr)
 library(here)
 library(zoo)
 library(ggtext)
+library(viridis)
 
 ar2_data <- read_csv(here("data","AR (2) - Foia JAN 24 JAN 29 RANGE.csv"),
                      col_types = cols(.default = "c"))
@@ -22,14 +23,22 @@ ar5_data <- read_csv(here("data", "AR FOIA (5) - AR FOIA (5).csv"),
                      col_types = cols(.default = "c"))
 ar6_data <- read_csv(here("data", "AR (6) - FOIA jan 31 feb 6th range (1).csv"), 
                      col_types = cols(.default = "c"))
+crosswalk <- read_csv(here("data", "cook_county_zip_city_crosswalk.csv"),
+                           col_types = cols(.default = "c"))
 
 colnames(ar2_data) <- c("zip", "date_time") # Are these first or second doses?
 colnames(ar3_data) <- c("zip", "date_time") # Are these first or second doses?
 colnames(ar4_data) <- c("zip", "date_time") # Note: these are SECOND doses (has extra row)
 colnames(ar5_data) <- c("zip", "date_time") # Are these first or second doses?
 colnames(ar6_data) <- c("zip", "date_time") # Note: these are FIRST doses (has extra row)
+colnames(crosswalk) <- c("zip", "city", "empty")
 
 # Clean up the data
+
+crosswalk <- crosswalk %>% 
+  select(zip, city)
+
+# For the rest of the data
 cleanData <- function(data, extraRow=FALSE) {
   if (extraRow) {
     data = data[-1,]
@@ -57,9 +66,61 @@ ar4_data.clean <- cleanData(ar4_data, extraRow=TRUE)
 ar5_data.clean <- cleanData(ar5_data)
 ar6_data.clean <- cleanData(ar6_data, extraRow=TRUE)
 
-# Look at unique ZIP codes
-unique(jan_data.clean$zip)
+# Merge the data
+full <- rbind(ar2_data.clean, ar3_data.clean, ar4_data.clean, ar5_data.clean, ar6_data.clean) %>% 
+  merge(crosswalk) %>% 
+  distinct() %>% 
+  arrange(zip, date) 
 
 # Look at just Cicero
-cicero_data <- jan_data.clean %>% 
-  filter(zip == "60804")
+cicero <- full %>% 
+  filter(city == "Cicero")
+
+ggplot(data=cicero, aes(date, vaccines)) +
+  geom_bar(stat = "identity") 
+
+# Look at all cities 
+unique(full$city)
+
+# How many total vacciones doses do we have in these datasets?
+sum_doses <- sum(full.total$total) # 8496
+
+# Compare cities total
+full.total <- full %>% 
+  group_by(city, date) %>% 
+  summarize(total = sum(vaccines)) %>% 
+  arrange(date, desc(total))
+
+cicero_vs_other <- full.total %>% 
+  mutate(cicero = case_when(city == "Cicero" ~ "Cicero",
+                            city != "Cicero" ~ "Other",
+                            is.na(city) ~ "NA")) %>% 
+  group_by(cicero, date) %>% 
+  summarize(total = sum(total)) %>% 
+  arrange(cicero, date)
+
+ggplot(data=cicero_vs_other, 
+       aes(date, total, fill=factor(cicero, levels=c("NA","Other", "Cicero")))) +
+  geom_bar(position="fill", stat="identity") + 
+  scale_fill_viridis(discrete = T, name="", direction=-1) +
+  scale_y_continuous(labels = scales::percent_format(accuracy=1), 
+                     breaks=seq(0, 1, 0.25)) +
+  xlab("Date") + ylab("Percent of total doses that day, by city") +
+  theme(
+    axis.ticks = element_blank(),
+    panel.grid.major.y = element_line(size=0.5, linetype="dotted", colour ="lightgrey"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_rect(fill = "white", colour = "white"))
+
+# Try a streamgraph?
+# Try a circular barplot? 
+
+
+# Questions
+# - We can only add these up if we think of them as just doses, and not first or second. Does first or second dose matter for this analysis?
+# - Do missing dates mean no vaccines were given or that we just don't have the data?
+# Are these the ZIP codes of all the people who received vaccines in Cicero?
+# What are the main suburbs we want to compare Cicero to?
+
